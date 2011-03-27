b352276ceee820d3ff38b7cf882eeff6e9a4dbf6
Mathias Paumgarten <mail@mathias-paumgarten.com>
2011-03-27 22:02:17 +0200

    adds multiple consumer and consumer can transport any fileformat

--- Consumer.cpp
+++ Consumer.cpp
@@ -11,47 +11,66 @@ void Consumer::run() {
         char requestBuffer[1024];
 		int requestSize = socket->receive(boost::asio::buffer(requestBuffer, 1024));
 
-		
+		std::stringstream httpResponse;
 		std::stringstream httpRequest(std::string(requestBuffer, requestSize));
+
 		//std::cout << httpRequest.str();
 		std::string method, filename;
 
-		
 		httpRequest >> method >> filename;
 		std::cout << "Requested file: " << filename << std::endl;
 
         filename.erase(0, 1);
 
-        std::stringstream stream;
-        std::ifstream file(filename);
-        std::string line;
+        boost::filesystem::path path(filename.c_str());
 
-        if (file.is_open()) {
+        if (boost::filesystem::is_regular_file(path)) {
 
-            while (file.good()) {
-                getline(file, line);
-                stream << line << "\n";
+            httpResponse << "HTTP/1.0 200 OK\n";
+		    httpResponse << "Server: FileServer/0.0.1\n";
+		    httpResponse << "Content-Type: application/octet-stream\n";
+            httpResponse << "Content-Length: " << boost::filesystem::file_size(path) << "\n\n";
+
+            socket->send(boost::asio::buffer(httpResponse.str().c_str(), httpResponse.str().length()));
+
+            std::ifstream file(filename.c_str(), std::ios::binary);
+
+            unsigned int bufferSize = 1024;
+            unsigned int currentPosition = 0;
+            char* buffer = new char[bufferSize];
+
+            while (!file.eof()) {
+                memset(buffer, 0, bufferSize);
+                file.read(buffer, bufferSize);
+
+                currentPosition += (unsigned int) file.gcount(); 
+
+                socket->send(boost::asio::buffer(buffer, bufferSize));
             }
 
             file.close();
+
+            delete buffer;
+
+        } else {
+
+            std::string errorMessage = "<!DOCTYPE html><html><head><title>Error</title></head><body>The file " + filename + " does not exist!</body></html>";
+
+            httpResponse << "HTTP/1.0 200 OK\n";
+		    httpResponse << "Server: FileServer/0.0.1\n";
+		    httpResponse << "Content-Type: text/html\n";
+		    httpResponse << "Content-Length: " << errorMessage.length() << "\n\n";
+
+            socket->send(boost::asio::buffer(httpResponse.str().c_str(), httpResponse.str().length()));
+
+            socket->send(boost::asio::buffer(errorMessage.c_str(), errorMessage.length()));
+
         }
-
-        std::string payload(stream.str());
-		std::stringstream httpResponse;
-
-        int payloadSize = payload.length();
-
-		httpResponse << "HTTP/1.0 200 OK\n";
-		httpResponse << "Server: FileServer/0.0.1\n";
-		httpResponse << "Content-Type: application/octet-stream\n";
-		httpResponse << "Content-Length: " << payloadSize << "\n\n";
 		
-		socket->send(boost::asio::buffer(httpResponse.str().c_str(), httpResponse.str().length()));
-
-		socket->send(boost::asio::buffer(payload.c_str(), payloadSize));
 		socket->shutdown(tcp::socket::shutdown_both);
 		socket->close();
 
         delete socket;
+        
     }
 }
--- main.cpp
+++ main.cpp
@@ -8,18 +8,22 @@ using namespace std;
 
 int main() {
     
-    cout << "Initializing buffer" << endl;
+    cout << "Initializing socket buffer" << endl;
     BoundedBuffer socketsBuffer(100);
 
     cout << "Initializing producer" << endl;
     Producer producer(80, &socketsBuffer);
 
-    cout << "Initializing consumer" << endl;
-    Consumer consumer(&socketsBuffer);
 
-    boost::thread consumerThread(boost::bind(&Consumer::run, &consumer));
+    cout << "Initializing multiple consumer:" << endl << endl;
+    for (int i = 0; i < 20; i++) {
+        cout << "    Starting consumer " << (i + 1) << endl;
 
-    cout << "Starting producer" << endl;
+        Consumer consumer(&socketsBuffer);
+        boost::thread consumerThread(boost::bind(&Consumer::run, &consumer));
+    }
+
+    cout << endl << "Starting producer" << endl;
     producer.run();
 
     return 0;
